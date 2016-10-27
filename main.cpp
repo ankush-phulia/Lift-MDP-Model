09cfb9ffccb776b8d6350735c01e934ceebe5d85
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2016-10-27 18:27:58 +0530

    State evaluation functions added for elevator

--- main.cpp
+++ main.cpp
@@ -2,6 +2,78 @@
 
 using namespace std;
 
+// Store the state space
+	std::bitset<126> mystate;
+
+bool floorbuttonpressed(int n) {
+	if(mystate[n+21])
+		return true;
+	else
+		return false;
+}
+
+bool elevatorbuttonpressed(int n, int k) {
+	if(k == 1) {
+		if(mystate[11+n])
+			return true;
+		else
+			return false;
+	}
+	else {
+		if(mystate[16+n])
+			return true;
+		else
+			return false;
+	}
+}
+
+bool doors_opened(int k) {
+	if(k == 1) {
+		if(mystate[10]) 
+			return true;
+		else
+			return false;
+	}
+	else {
+		if(mystate[11]) 
+			return true;
+		else
+			return false;
+	}
+}
+
+int get_direction(int k) {
+	int direction;
+	if(k == 1) {
+		if(mystate[6])
+			direction = -1;
+		else if(mystate[7])
+			direction = 1;
+		else
+			direction = 0;
+	}
+	else {
+		if(mystate[8])
+			direction = -1;
+		else if(mystate[9])
+			direction = 1;
+		else
+			direction = 0;
+	}
+	return direction;
+}
+
+int get_floor(int k) {
+	int floor;
+	if(k == 1) {
+		floor = mystate[0] + mystate[1]*2 + mystate[2]*4;
+	}
+	else {
+		floor = mystate[3] + mystate[4]*2 + mystate[5]*4;
+	}
+	return floor;
+}
+
 int main() {
 	cout<<"Lets get started Ankush!";
 	// State Space will be a bitset or a string of bits for smaller storage and easy accessability
@@ -21,7 +93,5 @@ int main() {
 			// 4 bits to hold wait time
 			// 3 bits to hold location
 
-	// Store the state space
-	std::bitset<126> mystate;
 	return 0;
 }
