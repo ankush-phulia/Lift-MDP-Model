49764d39b2632c80f6f30b35bc745b394970aca5
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2016-10-27 17:25:36 +0530

    Defined state space

--- main.cpp
+++ main.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main() {
 	cout<<"Lets get started Ankush!";
+	// State Space will be a bitset or a string of bits for smaller storage and easy accessability
+		// We need to decide the size of our bitset
+		  // ELEVATOR SPACE (Total 36 Bits)
+			// 5 bits for floor number of elevator 1
+			// 5 bits for floor number of elevator 2
+			// 8 bits for buttons on each floor ( 1 + 2 + 2 + 2 + 1 )
+			// 5 bits for buttons on elevator 1
+			// 5 bits for buttons on elevator 2
+			// 3 bits for direction of elevator 1
+			// 3 bits for direction of elevator 2
+			// 1 bit for opening of doors of elevator 1
+			// 1 bit for opening of doors of elevator 2
+		  // PERSON SPACE - Lets assume that there are a total of 10 people in the whole system (Each 7 bits)
+			// 1 bit to show presence in system
+			// 3 bits to hold wait time
+			// 3 bits to hold location
+
+	// Store the state space
+	std::bitset<106> mystate;
 	return 0;
 }
