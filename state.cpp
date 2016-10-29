4e0ce13bba941b78caf710d3d6b5a15bca3c1f78
Ankush-Phulia <ankush.phulia22@gmail.com>
2016-10-30 00:08:01 +0530

    Fresh start

--- state.cpp
+++ state.cpp
@@ -1,279 +1,55 @@
 #include "state.h"
 
-state::state(state new_state){
-	for(int i = 0; i < 200; i++) {
-		mystate[i] = new_state.mystate[i];
+state::state(int n){
+
+	this->elevator = 0;
+
+	this->floor_dist = new uint32_t [n];
+    for (int i = 0;i<n;i++){
+    	//initialise floor distribution probs
+    }
+
+    //initialise elevator distributions after this too
+    this->e0_dist = 0;
+    this->e1_dist = 0;
+
+    this->normalise = n*100;
+}
+
+void state::setFloor(int k, int n){
+	int mask = (4294967295)^(7<<(3*k));
+	elevator &= mask;
+	elevator |= (n<<(3*k));
+}
+
+
+void state::setDirection(int k, int dir){
+	//00 for static, 01 for up, 10 for down
+	int mask = (4294967295)^(3<<(23+2*k));
+	elevator &= mask;
+	elevator |= (dir<<(23+2*k));
+}
+
+void state::setProbElev(int k, int n, int newProb){
+	int mask = (4294967295)^(4095<<(12*n));
+	if (!k){
+		e0_dist &= mask;
+		e0_dist |= (newProb<<(12*n));
+	}
+	else{
+		e1_dist &= mask;
+		e1_dist |= (newProb<<(12*n));
 	}
 }
 
-void state::create_state(vector<string> action, state current_state) {
-	state new_state(current_state);
-
-	for(int i = 0; i < action.size(); i++) {
-		// A PERSON BEING ADDED
-		if(action[i][0] == 'P') {
-			if(action[i][1] == 'I') {
-				int person = (int)action[i][2] -'0';
-				int floor = (int)action[i][3] - '0';
-				new_state.add_person(person, floor);
-			}
-			else if(action[i][1] == 'O') {
-				int person = (int)action[i][2] - '0';
-				new_state.delete_person(person);
-			}
-			else if(action[i][1] == 'D') {
-				int person = (int)action[i][2] - '0';
-				int destination = (int)action[i][3] - '0';
-				new_state.update_person_destination(person, destination);
-			}
-			else if(action[i][1] == 'L') {
-				int person = (int)action[i][2] - '0';
-				int location = (int)action[i][3] - '0';
-				new_state.person_moves(person, location);
-			}
-		}
-
-		// A BUTTON BEING PRESSED
-		if(action[i][0] == 'B') {
-			if(action[i][1] == 'U') {
-				int floor = (int)action[i][2] - '0';
-				new_state.floor_button(2*floor-1, true);
-			}
-			else if(action[i][1] == 'D') {
-				int floor = (int)action[i][2] - '0';
-				new_state.floor_button(2*(floor-1), true);
-			}
-			else {
-				int floor = (int)action[i][1] - '0';
-				int elevator = (int)action[i][2] - '0';
-				new_state.lift_button(floor, elevator, true);
-			}
-		}
-
-		// LIFT MOVING
-		if(action[i][0] == 'A') {
-			if(action[i][1] == 'U') {
-				int elevator = (int)action[i][2] -'0';
-				new_state.elevator_moves(1, elevator);
-			}
-			else if(action[i][1] == 'D') {
-				int elevator = (int)action[i][2] -'0';
-				new_state.elevator_moves(-1, elevator);
-			}
-			else if(action[i][1] == 'S') {
-				int elevator = (int)action[i][2] -'0';
-				new_state.elevator_moves(0, elevator);
-			}
-			else if(action[i][1] == 'O') {
-				int elevator = (int)action[i][3] -'0';
-				if(action[i][2] == 'U') {
-					new_state.elevator_moves(1, elevator);
-				}
-				else if(action[i][2] == 'D') {
-					new_state.elevator_moves(-1, elevator);
-				}
-				new_state.elevator_doors(true, elevator);
-			}
-			else if(action[i][1] == 'C') {
-				int elevator = (int)action[i][3] -'0';
-				if(action[i][2] == 'U') {
-					new_state.elevator_moves(1, elevator);
-				}
-				else if(action[i][2] == 'D') {
-					new_state.elevator_moves(-1, elevator);
-				}
-				new_state.elevator_doors(false, elevator);
-			}
-			else {
-				int elevator = (int)action[i][1] -'0';
-				int floor = (int)action[i][2] -'0';
-				new_state.update_floor(floor, elevator);
-			}
-		}
-	}
-	for(int i = 0; i < no_of_people; i++) {
-		if(new_state.mystate[10+4*P+n*11])
-			new_state.update_person_time(i);
-	}
-	current_state.links.push_back(new_state);
+void state::setProbFloUp(int n, int newProb){
+	int mask = (4294967295)^(4095);
+	floor_dist[n] &= mask;
+	floor_dist[n] |= (newProb);
 }
 
-// PERFORM ACTION
-void state::perform_action(std::string action, state current_state) {
-	state new_state(current_state);
-	
+void state::setProbFloDown(int n, int newProb){
+	int mask = (4294967295)^(4095<<12);
+	floor_dist[n] &= mask;
+	floor_dist[n] |= (newProb<<12);
 }
-
-// FUNCTIONS TO GET STATE OF VARIABLES
-int state::get_destination(int n) {
-	int destination;
-	destination = mystate[18+4*P+n*11] + 2*mystate[19+4*P+n*11] + 4*mystate[20+4*P+n*11];
-	return destination;
-}
-
-int state::get_location(int n) {
-	int location;
-	location = mystate[15+4*P+n*11] + 2*mystate[16+4*P+n*11] + 4*mystate[17+4*P+n*11];
-	return location;
-}
-
-int state::get_waiting_time(int n) {
-	int time;
-	time = mystate[11+4*P+n*11] + 2*mystate[12+4*P+n*11] + 4*mystate[13+4*P+n*11] + 8*mystate[14+4*P+n*11];
-	return time;
-}
-
-bool state::does_person_exist(int n) {
-	if(mystate[10+4*P+n*11]) 
-		return true;
-	else
-		return false;
-}
-
-bool state::floor_button_pressed(int n) {
-	if(mystate[n+11+2*P])
-		return true;
-	else
-		return false;
-}
-
-bool state::elevator_button_pressed(int n, int k) {
-	if(k == 1) {
-		if(mystate[11+n])
-			return true;
-		else
-			return false;
-	}
-	else {
-		if(mystate[11+P+n])
-			return true;
-		else
-			return false;
-	}
-}
-
-bool state::doors_opened(int k) {
-	if(k == 1) {
-		if(mystate[10]) 
-			return true;
-		else
-			return false;
-	}
-	else {
-		if(mystate[11]) 
-			return true;
-		else
-			return false;
-	}
-}
-
-int state::get_direction(int k) {
-	int direction;
-	if(k == 1) {
-		if(mystate[6])
-			direction = -1;
-		else if(mystate[7])
-			direction = 1;
-		else
-			direction = 0;
-	}
-	else {
-		if(mystate[8])
-			direction = -1;
-		else if(mystate[9])
-			direction = 1;
-		else
-			direction = 0;
-	}
-	return direction;
-}
-
-int state::get_floor(int k) {
-	int floor;
-	if(k == 1)
-		floor = mystate[0] + mystate[1]*2 + mystate[2]*4;
-	else
-		floor = mystate[3] + mystate[4]*2 + mystate[5]*4;
-	return floor;
-}
-
-// FUNCTIONS TO UPDATE THE STATE OF VARIABLES
-void state::update_floor(int n, int k) {
-	for(int i = 2; i >= 0; i--) {
-		mystate[-3+k*3+i] = n%2;
-		n = n/2;
-	}
-}
-
-void state::elevator_moves(int dir, int k) {
-	bool down = false, up = false;
-	int index = 4 + 2*k;
-	if(dir == 1)
-		up = true;
-	else if(dir == -1)
-		down = true;
-	mystate[index] = down;
-	mystate[index+1] = up;
-}
-
-void state::elevator_doors(bool open, int k) {
-	if(open)
-		mystate[9+k] = true;
-	else
-		mystate[9+k] = false;
-}
-
-void state::lift_button(int n, int k, bool pressed) {
-	if(pressed) 
-		mystate[6+P*k+n] = true;
-	else
-		mystate[6+P*k+n] = false;
-}
-
-void state::floor_button(int n, bool pressed) {
-	if(pressed) 
-		mystate[11+2*P+n] = true;
-	else
-		mystate[11+2*P+n] = false;
-}
-
-void state::add_person(int n, int floor) {
-	mystate[10+4*P+n*11] = true;
-	for(int i = 0; i < 4; i++) {
-		mystate[11+4*P+n*11+i] = false;
-	}
-	for(int i = 2; i >= 0; i--) {
-		mystate[15+4*P+n*11+i] = floor%2;
-		floor = floor/2;
-	}
-}
-
-void state::delete_person(int n) {
-	for(int i = 0; i < 11; i++) {
-		mystate[10+4*P+n*11+i] = false;
-	}
-}
-
-void state::person_moves(int n, int loc) {
-	for(int i = 2; i >= 0; i--) {
-		mystate[15+4*P+n*11+i] = loc%2;
-		loc = loc/2;
-	}
-}
-
-void state::update_person_time(int n) {
-	int time = get_waiting_time(n);
-	time++;
-	for(int i = 3; i >= 0; i--) {
-		mystate[11+4*P+n*11+i] = time%2;
-		time = time/2;
-	}
-}
-
-void state::update_person_destination(int n, int floor) {
-	for(int i = 2; i >= 0; i--) {
-		mystate[18+4*P+n*11+i] = floor%2;
-		floor = floor/2;
-	}
-}
-
--- state.h
+++ state.h
@@ -9,36 +9,86 @@ using namespace std;
 class state{
 
 public:
-	bitset<200> mystate;
-	vector<state> links;
-	int P, Q, R;
+	uint32_t elevator;
+	//0-2: elevator 0 position
+	//3-5: elevator 1 position
+	//6 : button on 1st floor
+	//7-12: buttons on 2nd - 4th floor
+	//13: button on 5th floor
+	//14-18: elevator 0 buttons
+	//19-23: elevator 1 buttons
+	//24-25: e0 direction
+	//26-27: e1 direction
+	//28: e0 open = 1/closed
+	//29: e1 open = 1/closed
+	//etc
+	uint64_t e0_dist;
+	uint64_t e1_dist;
+	//prob distr. of ppl wanting to get off at each floor - 12 bits per floor
+	uint32_t *floor_dist;
+	//prob distr. of ppl wanting to go up or down on each floor - 12 bits per direction
+	int normalise;// normalising constant - 12 bit number
 
-	state(state new_state);
-	void create_state(vector<string> action, state current_state);
-	void perform_action(string action, state current_state);
+	state(int n);//constructor
 
-	//retrieval methods
-	int get_destination(int n);
-	int get_location(int n);
-	int get_waiting_time(int n);
-	bool does_person_exist(int n);
-	bool floor_button_pressed(int n);
-	bool elevator_button_pressed(int n, int k);
-	bool doors_opened(int k);
-	int get_direction(int k);
-	int get_floor(int k);
+	//methods for retrieving elevators
+	inline int getFloor(int k){
+		//elevator 0 or 1
+		return (elevator>>(3*k))&7;
+	}
+	inline bool isOpen(int k){
+		return (elevator&(1<<(28+k)))==1;
+	}
+	inline int getDirection(int k){
+		return (elevator>>(27+k))&3;
+	}
+	inline bool elevatorButtonPressed(int k, int n){
+		//k = 0,1 ; n = 0,1,2,3,4
+		return (elevator&(1<<(14+8*k+n)))==1;
+	}
 
-	//update methods
-	void update_floor(int n, int k);
-	void elevator_moves(int dir, int k);
-	void elevator_doors(bool open, int k);
-	void lift_button(int n, int k, bool pressed);
-	void floor_button(int n, bool pressed);
-	void add_person(int n, int floor);
-	void delete_person(int n);
-	void person_moves(int n, int loc);
-	void updsonate_per_time(int n);
-	void update_person_destination(int n, int floor);	
+	//methods for changing elevators
+	void setFloor(int k, int n);
+
+	inline void toggleDoor(int k){
+		//open-> close and close->open
+		elevator ^= (1<<(28+k));
+	}
+
+	//00 for static, 01 for up, 10 for down
+	inline void setDirection(int k, int dir);
+
+	inline void toggleFloorButton(int n, int upordown){
+		//make upordown bool??!!
+		//careful when using button for floor 1 or 5, always give upordown as 0 or false
+		elevator ^= (1<<(6+n+upordown));
+	}
+
+	inline void toggleElevatorButton(int n, int k){
+		elevator ^= (1<<(14+8*k+n));
+	}
+
+	//methods for retrieving distribution
+	inline float getProbElev(int n,int k){
+		//prob a person in elevator k gets off at floor n
+		return (k==0)?(((e0_dist>>(12*n))&(4095))/normalise):(((e1_dist>>(12*n))&(4095))/normalise);
+	}
+
+	inline float getProbFloUp(int n){
+		//prob that a person on floor n wants to go up
+		return (floor_dist[n]&(4095))/normalise;
+	}
+
+	inline float getProbFloDown(int n){
+		//prob that a perosn on floor n wants to go down
+		return ((floor_dist[n]>>12)&(4095))/normalise;
+	}
+
+	//methods for changing distributions
+	void setProbElev(int k, int n, int newProb);
+	void setProbFloUp(int n, int newProb);
+	void setProbFloDown(int n, int newProb);
+		
 };
 
 #endif
